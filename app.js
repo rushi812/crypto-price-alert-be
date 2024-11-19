49ad691384d25fe23d636dfd2c0b537f50d71526
Rushiraj Brahmbhatt <rushibrahmbhatt33@gmail.com>
2024-11-19 21:53:52 +0530

    CORS error fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const connectDB = require("./utils/db");
 const subscriptionRoutes = require("./routes/subscriptionRoutes");
 require("dotenv").config();
@@ -9,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
