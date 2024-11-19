616f22b1ef05c71442d72609b83b4680857556d1
Rushiraj Brahmbhatt <rushibrahmbhatt33@gmail.com>
2024-11-19 14:59:03 +0530

    Send email only once condition added

--- services/cronService.js
+++ services/cronService.js
@@ -14,24 +14,31 @@ cron.schedule("* * * * *", async () => {
     // Fetch the current price from the external API
     const response = await axios.get(PRICE_API_URL);
     const currencies = response.data;
+    const ids = [];
 
     // Fetch all subscriptions from the database
-    const subscriptions = await Subscription.find();
+    const subscriptions = await Subscription.find({ alertSent: false });
 
     for (const sub of subscriptions) {
       const currency = currencies.find((item) => item.id === sub.id);
       if (
-        currency.current_price >= sub.price ||
-        currency.current_price <= sub.price
+        (currency.current_price >= sub.price ||
+          currency.current_price <= sub.price)
       ) {
         const message = `Price Alert: The current price is ${currency.current_price}, which meets your target of ${sub.price}.`;
         await sendEmail(sub.email, "Price Alert Notification", message);
         console.log(`Email sent to ${sub.email} (CRON)`);
+        ids.push(sub._id);
       }
+    }
+
+    if (!!ids.length) {
+      await Subscription.updateMany(
+        { _id: { $in: ids } },
+        { $set: { alertSent: true } }
+      );
     }
   } catch (error) {
     console.error("Error in price check job:", error.message);
   }
 });
-
-module.exports = cron;
