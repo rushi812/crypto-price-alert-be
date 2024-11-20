ab693fbaa80c825c68978e6243293501d67ac257
Rushiraj Brahmbhatt <rushibrahmbhatt33@gmail.com>
2024-11-20 13:22:36 +0530

    Server origin fix

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const connectDB = require("./utils/db");
 const subscriptionRoutes = require("./routes/subscriptionRoutes");
@@ -10,9 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'https://price-alert-812.netlify.app',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
+app.use(express.json());
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
